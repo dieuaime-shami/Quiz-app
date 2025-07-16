ab9c22276ee49830b7b0716bf53df1932dc6a630
dieuaime-shami <shamidieuaime272@gmail.com>
2025-07-16 11:58:26 +0200

    the questions have been adde (in js file)

---
+++ script.js
@@ -0,0 +1,39 @@
+const questions = [
+  {
+    question: "What is the biggest lake in Rwanda?",
+    answers: [
+      { text: "Ruhondo", correct: false },
+      { text: "Mugesera", correct: false },
+      { text: "Kivu", correct: true },
+      { text: "Muhazi", correct: false }
+    ]
+  },
+  {
+    question: "Which city is the capital of Rwanda?",
+    answers: [
+      { text: "Huye", correct: false },
+      { text: "Musanze", correct: false },
+      { text: "Kigali", correct: true },
+      { text: "Rubavu", correct: false }
+    ]
+  },
+  {
+    question: "Which animal is featured in Rwanda's tourism slogan 'Tembera u Rwanda'?",
+    answers: [
+      { text: "Elephant", correct: false },
+      { text: "Lion", correct: false },
+      { text: "Gorilla", correct: true },
+      { text: "Zebra", correct: false }
+    ]
+  },
+  {
+    question: "What is Rwanda's official language alongside English and French?",
+    answers: [
+      { text: "Zulu", correct: false },
+      { text: "Kinyarwanda", correct: true },
+      { text: "Swahili", correct: false },
+      { text: "Luganda", correct: false }
+    ]
+  }
+];
+
